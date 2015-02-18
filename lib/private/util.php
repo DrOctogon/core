6e7a85dfe9a6857ca3b0997126ef1412985f9fca
Thomas Müller <thomas.mueller@tmit.eu>
2015-02-18 18:35:27 +0100

    deprecate isValidFileName()

--- lib/private/util.php
+++ lib/private/util.php
@@ -1463,6 +1463,7 @@ class OC_Util {
 	 *
 	 * @param string $file file name to check
 	 * @return bool true if the file name is valid, false otherwise
+	 * @deprecated use \OC\Files\View::verifyPath()
 	 */
 	public static function isValidFileName($file) {
 		$trimmed = trim($file);
